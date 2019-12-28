70be0713d3ecba9777a27c5c6b48fc56a4fb955d
Ebbe A <ebbe.arildsson@gmail.com>
2019-12-28 19:05:32 +0100

    speed increase

--- js/strings.js
+++ js/strings.js
@@ -1,31 +1,49 @@
 let circles = []
-let speed = 0.1
+let speed = 1
 let amount = 200
+let size = 150
+let sliderA
+let sliderS
 
 function setup(){
     createCanvas(innerWidth, innerHeight)
     background(0)
     strokeWeight(0.01)
+    sliderS = createSlider(10, 1000, 150, 10)
+    sliderA = createSlider(10, 1000, 200, 10)
+    sliderS.position(10, 10)
+    sliderA.position(10, 30)
+    getCircles()
+}
+
+function getCircles(){
+    circles = []
     for(let i = 0; i < amount; i++){
-        circles.push(new Circle(random(0, width), random(0, height), random(-speed, speed), random(-speed, speed)))
+        circles.push(new Circle(random(0, width), random(0, height), random(-speed, speed), random(-speed, speed), size))
     }
 }
 
 function draw(){
+    if(sliderS.value() !== size || sliderA.value() !== amount){
+        size = sliderS.value()
+        amount = sliderA.value()
+        background(0)
+        getCircles()
+    }
+
     circles.forEach(c => {
         c.close(circles)
         c.move()
         c.draw()
     })
 }
-
 class Circle{
-    constructor(x, y, xs, ys){
+    constructor(x, y, xs, ys, s){
         this.x = x
         this.y = y
         this.xs = xs
         this.ys = ys
-        this.s = 150
+        this.s = s
         this.c = []
     }
 
@@ -48,6 +66,13 @@ class Circle{
         noFill()
         stroke(255, 255, 255)
         //ellipse(this.x, this.y, this.s)
+        /*
+        if(this.c.length > 1) this.c.forEach(c => { 
+            let len = map(sqrt(sq(c.x-this.x) + sq(c.y-this.y)), 0, 150, 255, 0)
+            stroke(len)
+            line(this.x, this.y, c.x, c.y)
+        })
+        */
         if(this.c.length > 1) this.c.forEach(c => { 
             let len = map(sqrt(sq(c.x-this.x) + sq(c.y-this.y)), 0, 150, 255, 0)
             stroke(len, 100 + map(len, 0, 255, 0, 50), 255 -len)
