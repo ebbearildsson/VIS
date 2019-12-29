7f93ecba1bce70d3b2ec5cded48d60065fd8b9f0
Ebbe A <ebbe.arildsson@gmail.com>
2019-12-29 15:45:38 +0100

    Added more functionality to strings.js

--- js/strings.js
+++ js/strings.js
@@ -1,19 +1,51 @@
 let circles = []
-let speed = 1
+let speed = 0.8
 let amount = 200
 let size = 150
 let sliderA
 let sliderS
+let checkW
+let checkM
+let checkP
+let web = true
+let mono = false
+let paused = false
 
 function setup(){
     createCanvas(innerWidth, innerHeight)
     background(0)
-    strokeWeight(0.01)
+
     sliderS = createSlider(10, 1000, 150, 10)
     sliderA = createSlider(10, 1000, 200, 10)
+    checkW = createCheckbox('Web', true)
+    checkM = createCheckbox('Monochromatic', false)
+    checkP = createCheckbox('Pause', false)
+
+    checkW.changed(checkWEvent)
+    checkM.changed(checkMEvent)
+    checkP.changed(checkPEvent)
+
     sliderS.position(10, 10)
     sliderA.position(10, 30)
+    checkW.position(10, 50)
+    checkM.position(10, 70)
+    checkP.position(10, 90)
+
     getCircles()
+}
+
+function checkWEvent(){
+    web = !web
+    background(0)
+}
+
+function checkMEvent(){
+    mono = !mono
+    background(0)
+}
+
+function checkPEvent(){
+    paused = !paused
 }
 
 function getCircles(){
@@ -24,18 +56,27 @@ function getCircles(){
 }
 
 function draw(){
-    if(sliderS.value() !== size || sliderA.value() !== amount){
-        size = sliderS.value()
-        amount = sliderA.value()
-        background(0)
-        getCircles()
+    if(!paused){
+        if(!web) {
+            background(0)
+            strokeWeight(0.5)
+        } else {
+            strokeWeight(0.01)
+        }
+    
+        if(sliderS.value() !== size || sliderA.value() !== amount){
+            size = sliderS.value()
+            amount = sliderA.value()
+            background(0)
+            getCircles()
+        }
+    
+        circles.forEach(c => {
+            c.close(circles)
+            c.move()
+            c.draw(mono)
+        })
     }
-
-    circles.forEach(c => {
-        c.close(circles)
-        c.move()
-        c.draw()
-    })
 }
 class Circle{
     constructor(x, y, xs, ys, s){
@@ -61,23 +102,24 @@ class Circle{
         })
     }
 
-    draw(){
+    draw(color){
         push()
         noFill()
         stroke(255, 255, 255)
         //ellipse(this.x, this.y, this.s)
-        /*
-        if(this.c.length > 1) this.c.forEach(c => { 
-            let len = map(sqrt(sq(c.x-this.x) + sq(c.y-this.y)), 0, 150, 255, 0)
-            stroke(len)
-            line(this.x, this.y, c.x, c.y)
-        })
-        */
-        if(this.c.length > 1) this.c.forEach(c => { 
-            let len = map(sqrt(sq(c.x-this.x) + sq(c.y-this.y)), 0, 150, 255, 0)
-            stroke(len, 100 + map(len, 0, 255, 0, 50), 255 -len)
-            line(this.x, this.y, c.x, c.y)
-        })
+        if(mono){
+            if(this.c.length > 1) this.c.forEach(c => { 
+                let len = map(sqrt(sq(c.x-this.x) + sq(c.y-this.y)), 0, 150, 255, 0)
+                stroke(len)
+                line(this.x, this.y, c.x, c.y)
+            })    
+        } else {
+            if(this.c.length > 1) this.c.forEach(c => { 
+                let len = map(sqrt(sq(c.x-this.x) + sq(c.y-this.y)), 0, 150, 255, 0)
+                stroke(len, 100 + map(len, 0, 255, 0, 50), 255 -len)
+                line(this.x, this.y, c.x, c.y)
+            })
+        }
         pop()
     }
 }
