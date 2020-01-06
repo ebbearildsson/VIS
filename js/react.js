2e6f5d5a61e45671b9e872d73c7e7d2c66cd2896
Ebbe A <ebbe.arildsson@gmail.com>
2020-01-06 14:15:09 +0100

    Added ms to react

--- js/react.js
+++ js/react.js
@@ -1,8 +1,9 @@
 let started = false
 let clicked = false
 let restart = false
+let once = false
 let count = 0
-let timer
+let timer, timeStart, timeEnd
 
 function setup(){
     createCanvas(innerWidth, innerHeight)
@@ -20,6 +21,7 @@ function mouseClicked(){
         clicked = false
         started = false
         restart = false
+        once = false
         count = 0
     } else {
         if(!started){
@@ -27,6 +29,7 @@ function mouseClicked(){
             timer = random(50, 500)
         } else if(count >= timer){
             clicked = true
+            timeEnd = millis()
         }
     }
 }
@@ -42,10 +45,15 @@ function draw(){
         if(count >= timer){
             background(100, 255, 100)
             text('Click!', width/2, height/2)
+            if(!once){
+                timeStart = millis()
+                once = true
+            }
         }
     } else if(started && clicked){
         background(255, 100, 100)
         text((count - timer).toFixed(0) + ' Frames', width/2, height/2)
+        text((timeEnd - timeStart).toFixed(0) + ' ms', width/2, height/2 + 50)
         restart = true      
     }
 }
