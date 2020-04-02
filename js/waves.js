c94771b059020344ad29f4fd7ff31ead3c05fad0
Ebbe A <ebbe.arildsson@gmail.com>
2020-04-02 17:35:43 +0200

    Moving waves

--- js/waves.js
+++ js/waves.js
@@ -1,6 +1,7 @@
 let amp = 50;
 let spacing = 3 * amp
 let waves = []
+let count = 0
 
 function setup(){
     createCanvas(innerWidth, innerHeight)
@@ -10,20 +11,23 @@ function setup(){
     waves.push(createVector(0.4, 2))
     waves.push(createVector(8, 90))
     waves.push(createVector(1, 90))
-    waves.push(createVector(0.06, 2))
-    waves.push(createVector(40, 90))
     noStroke()
 }
 
 function draw(){
     background(0)
-    for(let x = 0; x < width; x += 0.1){
+    push()
+    stroke(250, 150, 50)
+    line(0, spacing * 2.2, width, spacing * 2.2)
+    pop()
+    for(let x = 0; x < width; x += 1){
         let y = 0
         for(let i = 0; i < waves.length; i++){
             fill(250, 150, 50)
-            ellipse(x, spacing * (i + 3) + sin(waves[i].x * x + waves[i].y) * amp, 3)
-            y += sin(waves[i].x * x + waves[i].y)
+            ellipse(x, spacing * (i + 3) + sin(waves[i].x * x + waves[i].y + count) * amp, 3)
+            y += sin(waves[i].x * x + waves[i].y + count)
         }
         ellipse(x, spacing + map(y * amp, -amp * (waves.length - 1), amp * (waves.length - 1), -amp * 2.5, amp * 2.5), 2)
     }
+    count++
 }
