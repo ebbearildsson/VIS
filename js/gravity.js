fff59ac155eadf6e10bf51bb65f60b31fa8ad457
Ebbe A <ebbe.arildsson@gmail.com>
2020-04-20 11:55:20 +0200

    Added letters

--- js/gravity.js
+++ js/gravity.js
@@ -1,7 +1,9 @@
 let bodies = [];
-let max_velocity = 5;
-let min_size = 5;
-let max_size = 20;
+const max_velocity = 5;
+const G = 0.1;
+const min_size = 5;
+const max_size = 50;
+const bounce_coefficient = 0.8;
 
 function setup(){
     createCanvas(innerWidth, innerHeight);
@@ -13,14 +15,24 @@ function setup(){
 function getBodies(amount){
     let temp_bodies = []
     for(let i = 0; i < amount; i++){
-        temp_bodies.push(new Body(createVector(random(-max_velocity, max_velocity), random(-max_velocity, max_velocity)), createVector(random(0, width), random(0, height)), random(min_size, max_size)));
+        temp_bodies.push(new Body(
+            createVector(
+                random(-max_velocity, max_velocity), 
+                random(-max_velocity, max_velocity)
+            ), 
+            createVector(
+                random(0, width), 
+                random(0, height)
+            ), 
+            random(min_size, max_size)
+        ));
     }
     return temp_bodies;
 }
 
 function draw(){
     background(0)
-    bodies.forEach(b =>  b.gravity());
+    bodies.forEach(b => b.gravity());
     bodies.forEach(b => {
         b.update();
         b.draw();
@@ -32,40 +44,39 @@ class Body{
         this.c = createVector(random(100, 255), random(100, 255), random(100, 255));
         this.v = initial_velocity;
         this.pos = body_position;
-        this.s = body_size;
+        this.s = sq(body_size);
     }
 
     gravity(){
-        let G = 0.015;
         let acc = createVector(0, 0);
         bodies.forEach(b => {
             if(b !== this){
-                let f = (G * this.s * b.s) / dist(this.pos.x, this.pos.y, b.pos.x, b.pos.y);
+                let f = (G * this.s * b.s) / sq(dist(this.pos.x, this.pos.y, b.pos.x, b.pos.y));
                 let angle = atan2((this.pos.y - b.pos.y), (this.pos.x - b.pos.x));
                 let a = f / this.s;
 
                 acc.add(createVector(-a * cos(angle), -a * sin(angle)));
             }
-        })
+        });
         this.v.add(acc);
     }
 
     update(){
         if(this.pos.x >= width ) {
             this.pos.x = width;
-            this.v.x *= -1;
+            this.v.x *= -bounce_coefficient;
         }
         else if(this.pos.x <= 0) {
             this.pos.x = 0;
-            this.v.x *= -1;
+            this.v.x *= -bounce_coefficient;
         }
         if(this.pos.y >= height) {
             this.pos.y = height;
-            this.v.y *= -1;
+            this.v.y *= -bounce_coefficient;
         }
         else if(this.pos.y <= 0) {
             this.pos.y = 0;
-            this.v.y *= -1;
+            this.v.y *= -bounce_coefficient;
         }
 
         this.v = createVector(constrain(this.v.x, -max_velocity, max_velocity), constrain(this.v.y, -max_velocity, max_velocity));
@@ -74,6 +85,6 @@ class Body{
 
     draw(){
         fill(this.c.x, this.c.y, this.c.z);
-        ellipse(this.pos.x, this.pos.y, this.s);
+        ellipse(this.pos.x, this.pos.y, sqrt(this.s));
     }
 }
---
+++ js/letters.js
@@ -0,0 +1,87 @@
+let dots = [];
+let targets = [];
+let animating = false;
+const G = 1000;
+const bounce_coefficient = 1;
+const max_velocity = 5;
+
+function setup(){
+    createCanvas(innerWidth, innerHeight);
+    background(0);
+    noStroke();
+    fill(255);
+}
+
+function draw(){
+    background(0);
+    if(animating === true) {
+        dots.forEach(dot => {
+            dot.update();
+            dot.draw();
+        });
+    } else if(mouseIsPressed) targets.push(createVector(mouseX, mouseY));
+    targets.forEach(target => ellipse(target.x, target.y, 2));
+    text("Draw by holding down left click\nPress ENTER to start animation", 10, 10);
+}
+
+function keyPressed() {
+    if (keyCode === ENTER) {
+        if(animating === false){
+            getDots();
+        }
+        if(animating === true){
+            dots = [];
+            targets = [];
+        }
+        animating = !animating;
+    } 
+}
+
+function getDots(){
+    targets.forEach(target => dots.push(new Dot(createVector(random(0, width), random(0, height)), target)));
+}
+
+class Dot{
+    constructor(position, target){
+        this.pos = position;
+        this.vel = createVector(0, 0);
+        this.size = 10;
+        this.target = target;
+    }
+
+    update(){
+        if(dist(this.pos.x, this.pos.y, this.target.x, this.target.y) > 5){
+            let acc = createVector(0, 0);
+            let f = (G * this.size) / sq(dist(this.pos.x, this.pos.y, this.target.x, this.target.y));
+            let angle = atan2((this.pos.y - this.target.y), (this.pos.x - this.target.x));
+            let a = f / this.size;
+
+            acc.add(createVector(-a * cos(angle), -a * sin(angle)));
+            this.vel.add(acc);
+
+            if(this.pos.x >= width ) {
+                this.pos.x = width;
+                this.vel.x *= -bounce_coefficient;
+            }
+            else if(this.pos.x <= 0) {
+                this.pos.x = 0;
+                this.vel.x *= -bounce_coefficient;
+            }
+            if(this.pos.y >= height) {
+                this.pos.y = height;
+                this.vel.y *= -bounce_coefficient;
+            }
+            else if(this.pos.y <= 0) {
+                this.pos.y = 0;
+                this.vel.y *= -bounce_coefficient;
+            }
+
+            this.vel = createVector(constrain(this.vel.x, -max_velocity, max_velocity), constrain(this.vel.y, -max_velocity, max_velocity));
+            this.pos.add(this.vel);
+        }
+    }
+
+    draw(){
+        ellipse(this.pos.x, this.pos.y, this.size);
+    }
+}
