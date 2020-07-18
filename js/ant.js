0896d7a9c5aa05fa7b3225535385d23f3c45bee0
Ebbe A <ebbe.arildsson@gmail.com>
2020-07-18 16:45:51 +0200

    Fixed Ant

--- js/ant.js
+++ js/ant.js
@@ -1,14 +1,26 @@
-let ant, move, sliderN
-let dir = 0
-let step = 5
+let ant, move, sliderN, sliderS;
+let arr;
+let dir = 0;
+let step = 2;
 
 function setup(){
-    createCanvas(innerWidth, innerHeight)
-    ant = createVector(f(width / 2), f(height / 2))
-    sliderN = createSlider(1, 1000, 1, 1)
-    sliderN.position(10, 10)
+    createCanvas(innerWidth, innerHeight);
+    ant = createVector(f(width / 2), f(height / 2));
+    sliderN = createSlider(1, 1000, 1, 1);
+    sliderS = createSlider(1, 50, 5, 1);
+    sliderN.position(10, 10);
+    sliderS.position(10, 30);
+    arr = getField();
+    rectMode(CENTER);
+    noStroke();
+}
 
-    frameRate(5)
+getField = () => {
+    let array = new Array(f(width / step) * f(height / step));
+    array.forEach(p => {
+        p = false;
+    });
+    return array;
 }
 
 f = (value) => { return Math.floor(value / step) * step }
@@ -16,69 +28,75 @@ f = (value) => { return Math.floor(value / step) * step }
 function moveR(){
     switch(dir){
         case(0):
-            move = createVector(-step, 0)
-            dir = 3
-        break
+            move = createVector(-step, 0);
+            dir = 3;
+        break;
         case(1):
-            move = createVector(0, -step)
-            dir = 0
-        break
+            move = createVector(0, -step);
+            dir = 0;
+        break;
         case(2):
-            move = createVector(step, 0)
-            dir = 1
-        break
+            move = createVector(step, 0);
+            dir = 1;
+        break;
         case(3):
-            move = createVector(0, step)
-            dir = 2
-        break
+            move = createVector(0, step);
+            dir = 2;
+        break;
     }
 }
 
 function moveL(){
     switch(dir){
         case(0):
-            move = createVector(step, 0)
-            dir = 1
-        break
+            move = createVector(step, 0);
+            dir = 1;
+        break;
         case(1):
-            move = createVector(0, step)
-            dir = 2
-        break
+            move = createVector(0, step);
+            dir = 2;
+        break;
         case(2):
-            move = createVector(-step, 0)
-            dir = 3
-        break
+            move = createVector(-step, 0);
+            dir = 3;
+        break;
         case(3):
-            move = createVector(0, -step)
-            dir = 0
-        break
+            move = createVector(0, -step);
+            dir = 0;
+        break;
     }
 }
 
 function draw(){
+    if(sliderS.value() !== step){
+        step = sliderS.value();
+        arr = getField();
+        background(255);
+    }
     for(let n = 0; n < sliderN.value(); n++){
-        let stoodOnBlack = false
-        loadPixels()
-        let off = (ant.y * width + ant.x) * pixelDensity() * 4
-        if(pixels[off] + pixels[off + 1] + pixels[off + 2] < 100) stoodOnBlack = true
-        updatePixels()
-
-        console.log(stoodOnBlack)
+        let stoodOnBlack = false;
+        if(arr[ant.y * f(width) + ant.x] == true) stoodOnBlack = true;
 
         if(stoodOnBlack == true){ 
-            fill(255)
-            moveR()
+            fill(255);
+            arr[ant.y * f(width) + ant.x] = false;
+            moveR();
         }
         else {
-            fill(0)
-            moveL()
+            fill(0);
+            arr[ant.y * f(width) + ant.x] = true;
+            moveL();
         }
-        ellipse(ant.x, ant.y, step)
+        rect(ant.x, ant.y, step, step);
         
-        ant.add(move)
-        if(ant.x > width) ant.x = 0
-        else if(ant.x < 0) ant.x = f(width)
-        if(ant.y > height) ant.y = 0
-        else if(ant.y < 0) ant.y = f(height)
+        ant.add(move);
+        if(ant.x > width) ant.x = 0;
+        else if(ant.x < 0) ant.x = f(width);
+        if(ant.y > height) ant.y = 0;
+        else if(ant.y < 0) ant.y = f(height);
+        fill(255, 0, 0);
+        rect(ant.x, ant.y, step, step);
     }
+    fill(90)
+    text(`Moves per frame: ${sliderN.value()}\nSize (Pixels): ${sliderS.value()}`, 175, 27);
 }
--- js/solar.js
+++ js/solar.js
@@ -18,24 +18,24 @@ function getSystem(){
     if(height < width) max = height / 2
     else max = width / 2 
     // Moons
-    let moon     = new Mass('The Moon',  max * 0.005, max * 0.032, createVector(240, 240, 240), [])
-    let ganymede = new Mass('Ganymedes', max * 0.005, max * 0.075, createVector(240, 240, 240), [])
-    let callisto = new Mass('Callisto',  max * 0.005, max * 0.063, createVector(240, 240, 240), [])
-    let io       = new Mass('IO',        max * 0.002, max * 0.056, createVector(240, 240, 240), [])
-    let europa   = new Mass('Europa',    max * 0.002, max * 0.043, createVector(240, 240, 240), [])
-    let titan    = new Mass('Titan',     max * 0.005, max * 0.026, createVector(240, 240, 240), [])
-    let triton   = new Mass('Triton',    max * 0.005, max * 0.022, createVector(240, 240, 240), [])
+    let moon     = new Mass('The Moon',  max * 0.005, max * 0.032, color(240, 240, 240), [])
+    let ganymede = new Mass('Ganymedes', max * 0.005, max * 0.075, color(240, 240, 240), [])
+    let callisto = new Mass('Callisto',  max * 0.005, max * 0.063, color(240, 240, 240), [])
+    let io       = new Mass('IO',        max * 0.002, max * 0.056, color(240, 240, 240), [])
+    let europa   = new Mass('Europa',    max * 0.002, max * 0.043, color(240, 240, 240), [])
+    let titan    = new Mass('Titan',     max * 0.005, max * 0.026, color(240, 240, 240), [])
+    let triton   = new Mass('Triton',    max * 0.005, max * 0.022, color(240, 240, 240), [])
     // Planets
-    let mercury  = new Mass('Mercury',   max * 0.005, max * 0.10,  createVector(250, 50, 50),   [])
-    let venus    = new Mass('Venus',     max * 0.010, max * 0.15,  createVector(100, 50, 50),   [])
-    let earth    = new Mass('Earth',     max * 0.020, max * 0.25,  createVector(50, 150, 50),   [moon])
-    let mars     = new Mass('Mars',      max * 0.015, max * 0.35,  createVector(250, 150, 50),  [])
-    let jupiter  = new Mass('Jupiter',   max * 0.035, max * 0.60,  createVector(200, 200, 120), [ganymede, callisto, io, europa])
-    let saturn   = new Mass('Saturn',    max * 0.025, max * 0.73,  createVector(255, 200, 120), [titan])
-    let uranus   = new Mass('Uranus',    max * 0.025, max * 0.87,  createVector(150, 200, 200), [])
-    let neptune  = new Mass('Neptune',   max * 0.025, max * 0.95,  createVector(100, 100, 200), [triton])
+    let mercury  = new Mass('Mercury',   max * 0.005, max * 0.10,  color(250, 50, 50),   [])
+    let venus    = new Mass('Venus',     max * 0.010, max * 0.15,  color(100, 50, 50),   [])
+    let earth    = new Mass('Earth',     max * 0.020, max * 0.25,  color(50, 150, 50),   [moon])
+    let mars     = new Mass('Mars',      max * 0.015, max * 0.35,  color(250, 150, 50),  [])
+    let jupiter  = new Mass('Jupiter',   max * 0.035, max * 0.60,  color(200, 200, 120), [ganymede, callisto, io, europa])
+    let saturn   = new Mass('Saturn',    max * 0.025, max * 0.73,  color(255, 200, 120), [titan])
+    let uranus   = new Mass('Uranus',    max * 0.025, max * 0.87,  color(150, 200, 200), [])
+    let neptune  = new Mass('Neptune',   max * 0.025, max * 0.95,  color(100, 100, 200), [triton])
     // The sun
-    sun          = new Mass('The Sun',   max * 0.050,          0,  createVector(255, 255, 0),   [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune])
+    sun          = new Mass('The Sun',   max * 0.050,          0,  color(255, 255, 0),   [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune])
 }
 
 function windowResized() {
@@ -57,7 +57,7 @@ class Mass{
         this.m = moons
         this.n = name
         this.s = size
-        this.a = 0
+        this.a = random(0, TWO_PI)
         this.x = 0
         this.y = 0
         this.w = 0
@@ -81,7 +81,7 @@ class Mass{
         }
 
         noStroke()
-        fill(this.c.x, this.c.y, this.c.z)
+        fill(this.c)
         ellipse(this.x, this.y, this.s)
         this.m.forEach(moon => moon.draw(this.x, this.y))
     }
