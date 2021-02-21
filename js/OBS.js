a2ed2e7b3098aa3d5c6b9052a2d0a14e2059d596
Ebbe A <ebbe.arildsson@gmail.com>
2021-02-21 17:20:48 +0100

    minor fix again

--- js/OBS.js
+++ js/OBS.js
@@ -9,5 +9,5 @@ function setup(){
 function draw(){
 	background(0, 255, 0);
 	count += 0.1;
-	ellipse(size + size * cos(count), size + size * sin(count), size / 10, size / 10);
+	ellipse(size * 2 + size / 2 * cos(count), size * 2 + size / 2 * sin(count), size / 10, size / 10);
 }
